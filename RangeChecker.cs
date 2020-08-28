c3ef2056dd749e74799cf10e038720132acda824
Dixit <Shashank.Dixit@philips.com>
2020-08-28 13:40:28 +0530

    Added RangeChecker.cs

---
+++ RangeChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+
+
+    internal class RangeChecker
+    {
+        static internal bool CheckBpm(float bpm)
+        {
+            return (bpm < 70 ? false : bpm > 150 ? false : true);
+        }
+        static internal bool CheckSpo2(float spo2)
+        {
+            return (spo2 < 90 ? false : true);
+        }
+        static internal bool CheckRespRate(float respRate)
+        {
+            return (respRate < 30 ? false : respRate > 95 ? false : true);
+        }
+    }

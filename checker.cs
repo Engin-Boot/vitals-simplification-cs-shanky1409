c4a4e5978919cf7ba8e243b4db9a1399682dd1a7
Dixit <Shashank.Dixit@philips.com>
2020-08-31 18:03:30 +0530

    Code refactored

--- checker.cs
+++ checker.cs
@@ -5,14 +5,14 @@ class Checker
 {
     static int Main()
     {
-        ExpectationChecker.ExpectTrue(CheckVitalStatus(100, 95, 60));
-        ExpectationChecker.ExpectFalse(CheckVitalStatus(40, 91, 92));
-        ExpectationChecker.ExpectFalse(CheckVitalStatus(151, 92, 94));
-        ExpectationChecker.ExpectFalse(CheckVitalStatus(72, 80, 92)); 
-        ExpectationChecker.ExpectFalse(CheckVitalStatus(80, 92, 20)); 
-        ExpectationChecker.ExpectFalse(CheckVitalStatus(80, 92, 100)); 
-	    ExpectationChecker.ExpectTrue(CheckVitalStatus(70, 92, 95));  
-	    ExpectationChecker.ExpectTrue(CheckVitalStatus(150, 90, 30));  
+        ExpectationChecker.ExpectTrue(RangeChecker.CheckVitalStatus(100, 95, 60));
+        ExpectationChecker.ExpectFalse(RangeChecker.CheckVitalStatus(40, 91, 92));
+        ExpectationChecker.ExpectFalse(RangeChecker.CheckVitalStatus(151, 92, 94));
+        ExpectationChecker.ExpectFalse(RangeChecker.CheckVitalStatus(72, 80, 92)); 
+        ExpectationChecker.ExpectFalse(RangeChecker.CheckVitalStatus(80, 92, 20)); 
+        ExpectationChecker.ExpectFalse(RangeChecker.CheckVitalStatus(80, 92, 100)); 
+	    ExpectationChecker.ExpectTrue(RangeChecker.CheckVitalStatus(70, 92, 95));  
+	    ExpectationChecker.ExpectTrue(RangeChecker.CheckVitalStatus(150, 90, 30));  
         Console.WriteLine("All ok");
         return 0;
     }
